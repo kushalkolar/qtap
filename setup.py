6f1856af3ab254ce6db622c627acef7e14350a59
kushalkolar <kushalkolar@gmail.com>
2020-07-04 22:52:44 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qtap',
+    version='0.1.0',
+    packages=['qtap'],
+    url='https://github.com/kushalkolar/qtap',
+    license='GPL v3.0',
+    author='Kushal Kolar',
+    author_email='',
+    description='Automatic Qt parameter entry widgets using function signatures '
+)
