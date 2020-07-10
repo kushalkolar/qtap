676387b82cd36b55bec41764f7b889aa9b526e00
kushalkolar <kushalkolar@gmail.com>
2020-07-10 21:46:26 +0200

    include readme for pypi description

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='qtap',
-    version='0.1.0',
+    version='0.1.1',
     packages=['qtap'],
     url='https://github.com/kushalkolar/qtap',
     license='GPL v3.0',
     author='Kushal Kolar',
     author_email='kushalkolar@alumni.ubc.ca',
-    description='Automatic Qt parameter entry widgets using function signatures '
+    description='Automatic Qt parameter entry widgets using function signatures ',
+    long_description=long_description,
+    python_requires='>=3.6',
 )
